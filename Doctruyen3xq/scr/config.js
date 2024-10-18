f4e52fa2732e17aa360d797cc38766362219379b
Giangnguyenpt <Giang.nguyenhoang91@gmail.com>
2024-10-18 09:26:14 +0700

    Add files via upload

---
+++ Doctruyen3xq/scr/config.js
@@ -0,0 +1,8 @@
+let BASE_URL1 = 'https://www.toptruyenvn.pro';
+let BASE_URL = 'https://doctruyen3qmoi.pro'
+try {
+    if (CONFIG_URL) {
+        BASE_URL = CONFIG_URL;
+    }
+} catch (error) {
+}
