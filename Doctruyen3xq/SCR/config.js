af63fb166c5a7ad9bcfdfe3672a4a1aac2c2e798
Giangnguyenpt <Giang.nguyenhoang91@gmail.com>
2024-10-18 09:31:21 +0700

    Delete Doctruyen3xq/SCR directory

--- Doctruyen3xq/SCR/config.js
+++
@@ -1,8 +0,0 @@
-let BASE_URL1 = 'https://www.toptruyenvn.pro';
-let BASE_URL = 'https://doctruyen3qmoi.pro'
-try {
-    if (CONFIG_URL) {
-        BASE_URL = CONFIG_URL;
-    }
-} catch (error) {
-}
